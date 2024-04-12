ee10037bee2d116ef762b310eeff1ef6ea577c1d
DumeM2b <163656850+DumeM2b@users.noreply.github.com>
2024-04-12 18:30:22 +0200

    Update calculator.py for test pylint

--- calculator.py
+++ calculator.py
@@ -1,14 +1,41 @@
-def addition(a, b):
-  result=a+b
-  return result
+def addition(a: int, b: int) -> int:
+    """Addition function.
 
-def soustraction(a, b):
-  result=a-b
-  return result
+    Args:
+        a (int): First operand.
+        b (int): Second operand.
 
-def division(a, b):
-  if b!=0:
-    result=a/b
+    Returns:
+        int: Result of the addition.
+    """
+    result = a + b
     return result
-  else:
-    return "Imposible division by 0"
+
+def soustraction(a: int, b: int) -> int:
+    """Subtraction function.
+
+    Args:
+        a (int): First operand.
+        b (int): Second operand.
+
+    Returns:
+        int: Result of the subtraction.
+    """
+    result = a - b
+    return result
+
+def division(a: int, b: int) -> float:
+    """Division function.
+
+    Args:
+        a (int): Dividend.
+        b (int): Divisor.
+
+    Returns:
+        float: Result of the division if divisor is not zero, otherwise returns an error message.
+    """
+    if b != 0:
+        result = a / b
+        return result
+    else:
+        return "Impossible division by 0"
